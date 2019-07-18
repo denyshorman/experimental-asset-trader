ec3ec8aecec5108ac93b9a63d66200fa705b5d4b
Denys Horman <den.horman@gmail.com>
2019-07-18 23:50:41 +0300

    Updated PoloniexApi tests.

--- src/test/kotlin/com/gitlab/dhorman/cryptotrader/service/poloniex/PoloniexApiTest.kt
+++ src/test/kotlin/com/gitlab/dhorman/cryptotrader/service/poloniex/PoloniexApiTest.kt
@@ -1,19 +1,23 @@
 package com.gitlab.dhorman.cryptotrader.service.poloniex
 
 import com.gitlab.dhorman.cryptotrader.core.Market
+import com.gitlab.dhorman.cryptotrader.service.poloniex.core.buyBaseAmount
 import com.gitlab.dhorman.cryptotrader.service.poloniex.core.calcQuoteAmount
 import com.gitlab.dhorman.cryptotrader.service.poloniex.model.BuyOrderType
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.runBlocking
 import mu.KotlinLogging
-import org.junit.Ignore
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 import java.math.BigDecimal
+import kotlin.random.Random
 
 @SpringBootTest
 @ActiveProfiles(profiles = ["test"])
+@Disabled
 class PoloniexApiTest {
     private val logger = KotlinLogging.logger {}
 
@@ -21,14 +25,12 @@ class PoloniexApiTest {
     private lateinit var poloniexApi: PoloniexApi
 
     @Test
-    @Ignore
     fun `Get open orders for market USDC_ATOM`() = runBlocking {
         val openOrders = poloniexApi.openOrders(Market("USDC", "ATOM"))
         logger.info("Open orders received: $openOrders")
     }
 
     @Test
-    @Ignore
     fun `Buy USDT on USDC_USDC market`() = runBlocking {
         val price = BigDecimal("0.9966")
         val baseAmount = BigDecimal("10.14628578")
@@ -44,9 +46,47 @@ class PoloniexApiTest {
     }
 
     @Test
-    @Ignore
     fun `Get all balances`() = runBlocking {
         val balances = poloniexApi.completeBalances()
         logger.info("Complete balances $balances")
+    }
+
+    @Test
+    fun `Get trade private history`() = runBlocking {
+        val history = poloniexApi.tradeHistory(limit = 20)
+        logger.info("History $history")
+    }
+
+    @Test
+    fun `Listen for account notifications`() = runBlocking {
+        poloniexApi.accountNotificationStream.collect {
+            println(it)
+        }
+    }
+
+    @Test
+    fun `Buy test`() = runBlocking {
+        val initAmount = BigDecimal("1")
+
+        while (true) {
+            val price = BigDecimal(
+                "" +
+                        sequence { for (i in 0..3) yield(Random.nextInt(1, 7)) }.joinToString("") +
+                        "." +
+                        sequence { for (i in 0..7) yield(Random.nextInt(0, 10)) }.joinToString("")
+            )
+            val q = calcQuoteAmount(initAmount, price)
+
+            val b = buyBaseAmount(q, price)
+            if (b.compareTo(BigDecimal.ONE) != 0) continue
+
+            try {
+                logger.info("Placing order: $price, $q")
+                val res = poloniexApi.buy(Market("USDT", "BTC"), price, q, BuyOrderType.PostOnly)
+                poloniexApi.cancelOrder(res.orderId)
+            } catch (e: Throwable) {
+                logger.error("${e.message}: $price, $q, ${buyBaseAmount(q, price)}")
+            }
+        }
     }
 }
