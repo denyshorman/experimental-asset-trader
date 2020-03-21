36bdac93a9d5a5891bef4f06557230a821340880
Denys Horman <den.horman@gmail.com>
2020-03-21 16:07:37 +0200

    Adjusted rounding mode to calculate the worst case.

--- src/main/kotlin/com/gitlab/dhorman/cryptotrader/service/poloniex/core/InternalUtils.kt
+++ src/main/kotlin/com/gitlab/dhorman/cryptotrader/service/poloniex/core/InternalUtils.kt
@@ -14,12 +14,12 @@ fun calcQuoteAmount(baseAmount: BigDecimal, price: BigDecimal): BigDecimal {
 
 // -USDT
 fun buyBaseAmount(quoteAmount: BigDecimal, price: BigDecimal): BigDecimal {
-    return (quoteAmount * price).setScale(8, RoundingMode.HALF_EVEN)
+    return (quoteAmount * price).setScale(8, RoundingMode.UP)
 }
 
 // +ETH
 fun buyQuoteAmount(quoteAmount: BigDecimal, feeMultiplier: BigDecimal): BigDecimal {
-    return (quoteAmount * feeMultiplier).setScale(8, RoundingMode.HALF_EVEN)
+    return (quoteAmount * feeMultiplier).setScale(8, RoundingMode.DOWN)
 }
 
 // -ETH
@@ -29,5 +29,5 @@ fun sellQuoteAmount(quoteAmount: BigDecimal): BigDecimal {
 
 // +USDT
 fun sellBaseAmount(quoteAmount: BigDecimal, price: BigDecimal, feeMultiplier: BigDecimal): BigDecimal {
-    return ((quoteAmount * price).setScale(8, RoundingMode.DOWN) * feeMultiplier).setScale(8, RoundingMode.HALF_EVEN)
+    return ((quoteAmount * price).setScale(8, RoundingMode.DOWN) * feeMultiplier).setScale(8, RoundingMode.DOWN)
 }
