b1c65f58cff1a364dc92ba4f655199ea2cd6e6e2
Denys Horman <den.horman@gmail.com>
2020-06-11 22:39:20 +0300

    Improved jackson configuration.

--- src/main/kotlin/com/gitlab/dhorman/cryptotrader/config/JacksonConfig.kt
+++ src/main/kotlin/com/gitlab/dhorman/cryptotrader/config/JacksonConfig.kt
@@ -12,12 +12,18 @@ import com.gitlab.dhorman.cryptotrader.core.Market
 import io.vavr.jackson.datatype.VavrModule
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.env.Environment
 
 @Configuration
-class JacksonConfig(private val env: Environment) {
+class JacksonConfig {
     @Bean
-    fun defaultObjectMapper(): ObjectMapper {
+    fun coreJacksonModule(): SimpleModule {
+        val module = SimpleModule()
+        module.addKeyDeserializer(Market::class.java, Market.Companion.KeyDecoder())
+        return module
+    }
+
+    @Bean
+    fun defaultObjectMapper(modules: List<SimpleModule>): ObjectMapper {
         val mapper = ObjectMapper()
             .registerModule(KotlinModule())
             .registerModule(VavrModule())
@@ -27,16 +33,15 @@ class JacksonConfig(private val env: Environment) {
 
         mapper.configure(JsonParser.Feature.ALLOW_COMMENTS, true)
 
-        if (env.activeProfiles.contains("prod")) {
-            mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        if (System.getenv("JACKSON_FAIL_ON_UNKNOWN_PROPERTIES") == null) {
+            mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
         } else {
-            mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
+            mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true)
         }
 
-        // TODO: Extract module
-        val simpleModule = SimpleModule()
-        simpleModule.addKeyDeserializer(Market::class.java, Market.Companion.KeyDecoder())
-        mapper.registerModule(simpleModule)
+        modules.forEach { module ->
+            mapper.registerModule(module)
+        }
 
         return mapper
     }
